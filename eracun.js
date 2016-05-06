b17dab30ec56ea15921747c81ac0e146c5d95ad5
Peter Colaric <pc2612@student.uni-lj.si>
2016-05-06 19:03:01 +0000

    Zaključek dela na I. nalogi

--- eracun.js
+++ eracun.js
@@ -209,24 +209,31 @@ streznik.post('/prijava', function(zahteva, odgovor) {
     	  Phone, Fax, Email, SupportRepId) \
         VALUES (?,?,?,?,?,?,?,?,?,?,?,?)");
       //TODO: add fields and finalize
-      //stmt.run("", "", "", "", "", "", "", "", "", "", "", 3); 
-      //stmt.finalize();
+      stmt.run(polja["FirstName"],polja["LastName"],  polja["Company"],
+               polja["Address"],  polja["City"],      polja["State"], 
+               polja["Country"],  polja["PostalCode"],polja["Phone"],
+               polja["Fax"],      polja["Email"],      3); 
+      stmt.finalize();
     } catch (err) {
       napaka2 = true;
     }
-  
-    odgovor.end();
   });
-})
+  
+  vrniStranke(function(napaka1, stranke) {
+    vrniRacune(function(napaka2, racuni) {
+      odgovor.render('prijava', {sporocilo: "", seznamStrank: stranke, seznamRacunov: racuni});  
+    }); 
+  });
+});
 
 // Prikaz strani za prijavo
 streznik.get('/prijava', function(zahteva, odgovor) {
   vrniStranke(function(napaka1, stranke) {
       vrniRacune(function(napaka2, racuni) {
         odgovor.render('prijava', {sporocilo: "", seznamStrank: stranke, seznamRacunov: racuni});  
-      }) 
+      }); 
     });
-})
+});
 
 // Prikaz nakupovalne košarice za stranko
 streznik.post('/stranka', function(zahteva, odgovor) {
